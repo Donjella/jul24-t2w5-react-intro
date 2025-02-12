612b76d69efd006893b12817baffb9d877077874
Donjella <brendonang@gmail.com>
2025-02-12 20:51:33 +1100

    feat: code block package implemented

--- src/FruitList.jsx
+++ src/FruitList.jsx
@@ -1,3 +1,7 @@
+// var Tooltip = require("rc-tooltip"); // CommonJS
+// import Tooltip from "rc-tooltip"; // Modern JavaScript import/export syntax
+
+import { CopyBlock } from "react-code-blocks";
 
 let favouriteFruits = [
 	"papaya",
@@ -7,11 +11,32 @@ let favouriteFruits = [
 	"peaches"
 ];
 
+let exampleJavaScriptCode = `
+console.log("Hello world!");
+let someNumber = 1;
+console.log("Some number is: " + 1);
+`;
+
+
 export function FruitList(){
 
 	return(
 		<div className="fruitList">
-			<h1>Favourite Fruits:</h1>
+
+			{/* <Tooltip placement="top" trigger={["hover"]} overlay={<div> JUL24 Cohort&apos;s Favourite Fruits </div>}> */}
+				<h1>Favourite Fruits:</h1>
+			{/* </Tooltip> */}
+
+			{/*  */}
+
+			<CopyBlock 
+				text={exampleJavaScriptCode} 
+				language="javascript" 
+				showLineNumbers={true} 
+				startingLineNumber={1} 
+				wrapLongLines={true}  
+			/>
+			
 			
 			<ul>
 
